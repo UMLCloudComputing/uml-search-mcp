b5fa8a7e2f96dedd15dc3f40cdc22a02b0f6902c
Gurpranked <gurpreet.singh.rehal@outlook.com>
2026-06-11 13:10:30 -0400

    feat: Updated server code with people and website search tools

--- server.py
+++ server.py
@@ -2,6 +2,7 @@
 
 import os
 from typing import Optional, Annotated
+from typing_extensions import _AnnotatedAlias
 from pydantic import Field
 from fastmcp import FastMCP
 from fastmcp.server.lifespan import lifespan
@@ -9,12 +10,7 @@ from fastmcp.dependencies import CurrentContext
 from fastmcp.server.context import Context
 from starlette.requests import Request
 from starlette.responses import JSONResponse
-from umlsearch import (
-    Course,
-    API,
-    parse_catalog_courses_response,
-    get_subject_prefix_mapping,
-)
+from umlsearch import query_website, query_people
 
 # Configuration
 BROADCAST_ADDRESS = os.getenv("BROADCAST_ADDRESS", "127.0.0.1")
@@ -38,6 +34,38 @@ mcp_server = FastMCP("UML-Search-MCP-Server", lifespan=app_lifespan)
 async def health_check(request: Request) -> JSONResponse:
     # Add pre-flight checks here, none at the moment
     return JSONResponse({"status": "ok", "service": "uml-search-mcp"}, status_code=200)
+
+
+@mcp_server.tool()
+async def search_uml_people(
+    query: Annotated[str, "The search query"],
+    top_k: Annotated[int, "The number of results to provide"],
+):
+    """Search for people at UMass Lowell"""
+    results = await query_people(query, top_k)
+    return results
+
+
+@mcp_server.tool()
+async def search_uml_website(
+    query: Annotated[str, "The search query"],
+    top_k: Annotated[int, "The number of results to provide"],
+):
+    """Search the general UMass Lowell website"""
+    results = await query_website(query, top_k)
+    return results
+
+
+@mcp_server.tool()
+async def search_uml_places(query: str):
+    """Search for places across UMass Lowell"""
+    pass
+
+
+@mcp_server.tool()
+async def search_uml_news(query: str):
+    """Search for UMass Lowell news"""
+    pass
 
 
 if __name__ == "__main__":
