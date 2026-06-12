bf6b3dcf34bb2c0f690bcf204c2e5a2675b3bc3f
Gurpranked <gurpreet.singh.rehal@outlook.com>
2026-06-11 22:52:42 -0400

    feat: Updated server code with changes for all tools, implemented all tools

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 # server.py
 
 import os
+import httpx
 from typing import Optional, Annotated
 from typing_extensions import _AnnotatedAlias
 from pydantic import Field
@@ -10,10 +11,14 @@ from fastmcp.dependencies import CurrentContext
 from fastmcp.server.context import Context
 from starlette.requests import Request
 from starlette.responses import JSONResponse
-from umlsearch import query_website, query_people
+from umlsearch import query_website, query_people, query_place, query_news, process_url
 
 # Configuration
 BROADCAST_ADDRESS = os.getenv("BROADCAST_ADDRESS", "127.0.0.1")
+DOCLING_BASE_ADDRESS = os.getenv("DOCLING_BASE_ADDRESS")
+
+# Global and shared AsyncClient object used for url data parsing tool
+client = None
 
 
 @lifespan
@@ -22,9 +27,12 @@ async def app_lifespan(server):
     print("Starting server...")
     try:
         # Configure server persistent
+        global client
+        client = httpx.AsyncClient()
         yield
     finally:
         print("Shutting down server...")
+        await client.aclose()
 
 
 mcp_server = FastMCP("UML-Search-MCP-Server", lifespan=app_lifespan)
@@ -36,6 +44,7 @@ async def health_check(request: Request) -> JSONResponse:
     return JSONResponse({"status": "ok", "service": "uml-search-mcp"}, status_code=200)
 
 
+# DONE
 @mcp_server.tool()
 async def search_uml_people(
     query: Annotated[str, "The search query"],
@@ -46,6 +55,7 @@ async def search_uml_people(
     return results
 
 
+# DONE
 @mcp_server.tool()
 async def search_uml_website(
     query: Annotated[str, "The search query"],
@@ -56,16 +66,33 @@ async def search_uml_website(
     return results
 
 
+# DONE
 @mcp_server.tool()
-async def search_uml_places(query: str):
+async def search_uml_places(
+    query: Annotated[str, "The search query"],
+    top_k: Annotated[int, "The number of results to provide"],
+):
     """Search for places across UMass Lowell"""
-    pass
+    results = await query_place(query, top_k)
+    return results
 
 
 @mcp_server.tool()
-async def search_uml_news(query: str):
+async def search_uml_news(
+    query: Annotated[str, "The search query"],
+    top_k: Annotated[int, "The number of results to provide"],
+):
     """Search for UMass Lowell news"""
-    pass
+    results = await query_news(query, top_k)
+    return results
+
+
+# DONE
+@mcp_server.tool()
+async def parse_uml_url(url: Annotated[str, "The UMass Lowell domain URL to parse"]):
+    """Parse and return the markdown content from a URL from the UMass Lowell domain"""
+    result = await process_url(client, url, DOCLING_BASE_ADDRESS)
+    return result
 
 
 if __name__ == "__main__":
